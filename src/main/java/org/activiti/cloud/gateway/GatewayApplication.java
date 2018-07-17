68f7d95380ff68d3338f68e664a2fc917987c12e
Salaboy <salaboy@gmail.com>
2018-07-17 11:47:15 +0100

    removing home

--- src/main/java/org/activiti/cloud/gateway/GatewayApplication.java
+++ src/main/java/org/activiti/cloud/gateway/GatewayApplication.java
@@ -12,16 +12,11 @@ import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitio
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @EnableScheduling
 @EnableDiscoveryClient
 @SpringBootApplication
-@RestController
 public class GatewayApplication {
 
     private static final Log log = LogFactory.getLog(GatewayApplication.class);
@@ -45,10 +40,5 @@ public class GatewayApplication {
     @Bean
     RestTemplate restTemplate() {
         return new RestTemplate();
-    }
-
-    @RequestMapping(method = GET)
-    public String home() {
-        return "Welcome to the Gateway Home! \n Click here to see available the <a href=\"/actuator/gateway/routes\"> Routes </a>";
     }
 }
